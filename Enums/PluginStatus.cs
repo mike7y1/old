c4ec4b100dffd14944509dde70f4532098d8ec4d
Grandmother <mikebenis47@gmail.com>
2026-01-11 16:37:24 -0500

    Add files via upload

---
+++ Enums/PluginStatus.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: InvokedServer.Enums.PluginStatus
+// Assembly: HQ, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: C53BC0E5-A85B-41E9-8105-FC74ED122A9A
+// Assembly location: C:\Users\ian\Downloads\Telegram Desktop\fein\HQ.exe
+
+
+namespace InvokedServer.Enums
+{
+	public enum PluginStatus
+	{
+		Loaded,
+		Installing,
+		PluginFileNotFound,
+	}
+}
---
+++ Enums/TransferType.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: InvokedServer.Enums.TransferType
+// Assembly: HQ, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: C53BC0E5-A85B-41E9-8105-FC74ED122A9A
+// Assembly location: C:\Users\ian\Downloads\Telegram Desktop\fein\HQ.exe
+
+
+namespace InvokedServer.Enums
+{
+	public enum TransferType
+	{
+		Upload,
+		Download,
+	}
+}
